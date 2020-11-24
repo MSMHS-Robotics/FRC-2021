170565e3fb57464b42eaea4a5c0c3106e6ba3e89
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2020-11-24 08:15:00 -0600

    Created the climber per #3 Also created RocketDigitalInput_T, to simulate digital inputs when testing

--- src/main/java/frc/robot/rocket_utils/RocketDigitalInput_T.java
+++ src/main/java/frc/robot/rocket_utils/RocketDigitalInput_T.java
@@ -2,18 +2,45 @@ package frc.robot.rocket_utils;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
+/** A class to simulate a digital input */
 public class RocketDigitalInput_T extends DigitalInput {
     public int port;
     public boolean state = false;
 
+    /**
+     * Creates a new RocketDigitalInput_T
+     * @param port the channel the input is attached to
+     */
     public RocketDigitalInput_T(int port) {
         super(port);
         this.port = port;
     }
 
+    /**
+     * Gets the state of the digital input
+     * @return the state of the digital input
+     */
     @Override
     public boolean get() {
         return state;
+    }
+
+    /**
+     * Gets the channel/port that was passed to the constructor
+     * Check this against the other ports to make sure there are no conflicts in testing
+     * @return the channel of this input
+     */
+    @Override
+    public int getChannel() {
+        return port;
+    }
+
+    /**
+     * Sets the state of the input, used for simulation purposes
+     * @param state the state of the input
+     */
+    public void set(boolean state) {
+        this.state = state;
     }
 
     
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,25 +2,26 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.rocket_utils.RocketDigitalInput;
+import frc.robot.rocket_utils.RocketDigitalInput_T;
 import frc.robot.rocket_utils.RocketMotor;
 import frc.robot.rocket_utils.RocketTalon;
 import frc.robot.rocket_utils.RocketTalon_T;
 import frc.robot.Constants;
 
 /**
- * A subsystem to control the indexer/pass-through
+ * A subsystem to control the climber
  */
 public class Climber extends SubsystemBase {
     private RocketMotor climberMotor;
     private RocketMotor extendMotor;
-    private RocketDigitalInput limitSwitch;
+    private DigitalInput limitSwitch;
+    private Boolean extendClimber = false;
 
     public Climber(int climber_p, int climberExtend_p, int limitSwitch_p) {
         if (Constants.unitTests) {
             climberMotor = new RocketTalon_T(climber_p);
             extendMotor = new RocketTalon_T(climberExtend_p);
-            limitSwitch = new RocketLimitSwitch_T(limitSwitch_p);
+            limitSwitch = new RocketDigitalInput_T(limitSwitch_p);
         } else {
             climberMotor = new RocketTalon(climber_p);
             extendMotor = new RocketTalon(climberExtend_p);
@@ -28,36 +29,54 @@ public class Climber extends SubsystemBase {
         }
     }
 
-  /**
-   * Sets the climber to a given speed
-   * @param power the speed at which you want the climber to run (negative means reverse)
-   */
-  public void setClimber(double power) {
-    climberMotor.set(power); // != null is handled inside RocketUtils
-  }
+    /**
+     * Sets the climber to a given speed
+     * 
+     * @param power the speed at which you want the climber to run (negative means reverse)
+     */
+    public void setClimber(double power) {
+        climberMotor.set(power); // != null is handled inside RocketUtils
+    }
 
-  public void extend() {
-      extendMotor.set(1);
-  }
+    /**
+     * Extends the climber
+     */
+    public void extendClimber() {
+        extendClimber = true;
+    }
 
-  /**
-   * Resets the climber subsystem to its pre-start state
-   */
-  public void reset() {
-    climberMotor.set(0);
-    extendMotor.set(0);
-  }
+    /**
+     * Tells if the climber is extended or not
+     * @return a boolean representing if the climber is extended or not
+     */
+    public Boolean isClimberExtended() {
+        return limitSwitch.get();
+    }
 
-  /**
-   * the required isGood() method
-   * here we only have one motor so it's pretty easy
-   * @return a Boolean representing if the motors can be reached or not
-   */
-  public Boolean isGood() {
-    return climberMotor.isMotorNotNull() && extendMotor.isMotorNotNull(); // RocketUtils at work
-  }
+    /**
+     * Resets the climber subsystem to its pre-start state
+     */
+    public void reset() {
+        climberMotor.set(0);
+        extendMotor.set(0);
+    }
 
-  @Override
-  public void periodic() {
-  }
+    /**
+     * the required isGood() method
+     * 
+     * @return a Boolean representing if the motors can be reached or not
+     */
+    public Boolean isGood() {
+        return climberMotor.isMotorNotNull() && extendMotor.isMotorNotNull(); // RocketUtils at work
+    }
+
+    @Override
+    public void periodic() {
+        if(extendClimber) { // if we need to extend the climber
+            extendMotor.set(1); // extend it until
+            if(limitSwitch.get()) { // we reach the top
+                extendMotor.set(0); // and then stop it. Should lead to minor oscillating at the top
+            }
+        }
+    }
 }
